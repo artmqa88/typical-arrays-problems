75f0b071404f51eec9446f9b188769308d7d00f5
artmqa88 <artmqa@mail.ru>
2021-03-12 16:43:40 +0300

    init: complete task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-
 exports.min = function min (array) {
-  return 0;
-}
+  if ((typeof array === 'undefined') || (array.length === 0)) {
+    return 0;
+  } else {
+    let min = array[0];
+      for (let i = 1 ; i < array.length; i++) {
+        if (min > array[i]) {
+          min = array[i];
+        }
+      }
+      return min;
+    }
+  }
 
-exports.max = function max (array) {
-  return 0;
-}
+  exports.max = function max (array) {
+    if ((typeof array === 'undefined') || (array.length === 0)) {
+      return 0;
+    } else {
+      let max = array[0];
+      for (let i = 1 ; i < array.length; i++) {
+        if (max < array[i]) {
+          max = array[i];
+        }
+      }
+      return max;
+    }
+  }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+  exports.avg = function avg (array) {
+    if ((typeof array === 'undefined') || (array.length === 0)) {
+      return 0;
+    } else {
+      let avg = 0;
+      for (let i = 0 ; i < array.length; i++) {
+        avg += array[i];
+      }
+      return avg / array.length;
+    }
+  }
+
+
+
